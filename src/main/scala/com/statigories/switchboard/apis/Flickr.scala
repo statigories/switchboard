46179a0f962293ceed85b832d623849ed3c62228
Ryan Martin <rmartin@gilt.com>
2015-07-18 08:22:29 -0600

    Cleaning up some things with the Flickr api class

--- src/main/scala/com/statigories/switchboard/apis/Flickr.scala
+++ src/main/scala/com/statigories/switchboard/apis/Flickr.scala
@@ -1,16 +1,31 @@
 package com.statigories.switchboard.apis
 
+import java.util.Date
+
 import com.statigories.switchboard.common.OAuthClient
 import org.scribe.builder.api.FlickrApi
 
 trait Flickr extends OAuthClient {
   override val apiType = classOf[FlickrApi]
   override val rootEndpoint = "https://api.flickr.com/services/rest/"
+  protected val BaseParameters = Map(
+    "format" -> "json"
+  )
 
   def getActivity(userUniqueIdentifier: String): String = {
-    getResponse(userUniqueIdentifier, buildGetRequest(parameters = Map(
+    getResponse(userUniqueIdentifier, buildGetRequest(parameters = BaseParameters ++ Map(
       "method" -> "flickr.activity.userPhotos",
-      "timeframe" -> "2d"
+      "timeframe" -> "4h"
+    )))
+  }
+
+  // TODO: Clean up the dates; likely needs a time zone for the user.
+  def getPhotoCounts(userUniqueIdentifier: String): String = {
+    val now = (new Date).getTime
+    val oneDayMs: Long = 1000 * 60 * 60 * 24
+    getResponse(userUniqueIdentifier, buildGetRequest(parameters = BaseParameters ++ Map(
+      "method" -> "flickr.photos.getCounts",
+      "dates" -> s"${now - (3L * oneDayMs)},${now - (2L * oneDayMs)},${now - (1L * oneDayMs)},$now"
     )))
   }
 }
--- src/test/scala/com/statigories/switchboard/apis/FlickrTest.scala
+++ src/test/scala/com/statigories/switchboard/apis/FlickrTest.scala
@@ -1,15 +1,13 @@
-package com.statigories.switchboard
-
-import com.statigories.switchboard.apis.Flickr
-import com.statigories.switchboard.common.providers.DefaultConfigProvider
-import com.statigories.switchboard.common.providers.models.{Token => TokenConfig}
-import com.statigories.switchboard.persistors.token.{TokenType, InMemoryPersistor}
-
-import org.scalatest.{FlatSpec, Matchers}
-import org.scribe.model.Token
+package com.statigories.switchboard.apis
 
 import java.net.URL
 import java.util.UUID
+
+import com.statigories.switchboard.common.providers.DefaultConfigProvider
+import com.statigories.switchboard.common.providers.models.{Token => TokenConfig}
+import com.statigories.switchboard.persistors.token.{InMemoryPersistor, TokenType}
+import org.scalatest.{FlatSpec, Matchers}
+import org.scribe.model.Token
 
 class FlickrTest extends FlatSpec with Matchers {
   // Supply config via either ENV variables or system properties.
@@ -24,6 +22,14 @@ class FlickrTest extends FlatSpec with Matchers {
   "#getActivity" should "work" in {
     val userGuid = UUID.randomUUID().toString
     Flickr.persist(userGuid, accessToken, TokenType.Access)
-    Flickr.getActivity(userGuid) should include("stat=\"ok\"")
+    val activity = Flickr.getActivity(userGuid)
+    activity should include("\"stat\":\"ok\"")
+  }
+
+  "#getPhotoCounts" should "work" in {
+    val userGuid = UUID.randomUUID().toString
+    Flickr.persist(userGuid, accessToken, TokenType.Access)
+    val counts = Flickr.getPhotoCounts(userGuid)
+    counts should include("\"stat\":\"ok\"")
   }
 }
